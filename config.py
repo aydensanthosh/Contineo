58c4aa2695f1251e91d965268c73b0785cf1dfb1
Ayden <aydensanthosh@gmail.com>
2026-04-18 22:34:32 +0530

    Adding Lab subjects

--- config.py
+++ config.py
@@ -83,23 +83,23 @@ SUBJECT_CODE_TO_NAME_MAP = {
     "CSL801" : "DC Lab",
     
     #Subject for FE
-    
+    #"(LAB) was added to subjects without Mid Sem and End sem for a more accurate result.
     "25BSC11CS01" : "Matrices and Differential Calculus",
     "25BSC11CS02" : "Engineering Physics",
     "25ESC11CS01" : "Engineering Graphics",
     "25ESC11CS02" : "Programming Fundamentals",
-    "25PCC11CS01" : "Innovation and Design Thinking",
-    "25PCC11CS02" : "Essential Computing Skills for Engineers",
-    "25VSE11CS01" : "Measuring Instruments and Testing Tools",
-    "25AEC11CS01" : "Art of Communication",
-    "25BSC11CS03" : "Integral Calculus and Probability Theory",
     "25BSC11CS04" : "Engineering Chemistry",
-    "25ESC11CS03" : "Basic Electrical and Electronics Engineering",
-    "25ESC11CS04" : "Human Health Systems",
     "25PCC11CS03" : "Digital Electronics",
-    "25PCC11CS04" : "Essential Psychomotor Skills for Engineers",
-    "25VSE11CS02" : "Creative Coding in Python"
-    "25IKS11CS01" : "Indian Knowledge System "
+    "25ESC11CS03" : "Basic Electrical and Electronics Engineering",
+    "25BSC11CS03" : "Integral Calculus and Probability Theory",
+    "25PCC11CS01" : "Innovation and Design Thinking (LAB)",
+    "25PCC11CS02" : "Essential Computing Skills for Engineers (LAB)",
+    "25VSE11CS01" : "Measuring Instruments and Testing Tools (LAB)",
+    "25AEC11CS01" : "Art of Communication (LAB)",
+    "25ESC11CS04" : "Human Health Systems (LAB)",
+    "25PCC11CS04" : "Essential Psychomotor Skills for Engineers (LAB)",
+    "25VSE11CS02" : "Creative Coding in Python (LAB)",
+    "25IKS11CS01" : "Indian Knowledge System (LAB)"
 
 
 }
